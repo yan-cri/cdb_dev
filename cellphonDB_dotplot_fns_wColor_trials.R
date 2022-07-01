01789c705ac0da89d35fcc652b1cf21a499aff51
yoitsryan <rburczak@colgate.edu>
2022-07-01 11:03:37 -0400

    Look at lines 40-47. Found 3 duplicate rows in origina dataset

--- cellphonDB_dotplot_fns_wColor_trials.R
+++ cellphonDB_dotplot_fns_wColor_trials.R
@@ -11,7 +11,7 @@ library(dplyr)
 dot_plot = function(selected_rows = NULL, selected_rows.nameReorder = as.logical(F), selected.resSave = as.logical(F), selected.resFnamePrefix = NULL, 
                     selected_columns = NULL, selected_columns_rename = as.logical(F), subnames = NULL, filename = 'plot.pdf', 
                     nonSig.removal = as.logical(F), 
-                    width = NULL, height = NULL, means_path = './means.txt', pvalues_path = './pvalues.txt', 
+                    width = NULL, height = NULL, means_path = './test_data/means.txt', pvalues_path = './test_data/pvalues.txt', 
                     means_separator = '\t', pvalues_separator = '\t', output_extension = '.pdf', plotDir = getwd(), pvalue=0.05, min.mean = NULL, max.mean = NULL, yAxisCol = NULL, debug = as.logical(F)){
   ## read in 'pvalues_path' and 'means_path' as 'all_pval' and 'all_means'
   all_pval = read.table(pvalues_path, header=T, stringsAsFactors = F, sep=means_separator, comment.char = '', check.names=F)
@@ -35,6 +35,16 @@ dot_plot = function(selected_rows = NULL, selected_rows.nameReorder = as.logical
   sort(sorted_pval)
   # Look ma, no dupes!
   # So, the data must be clean, huh?
+  
+  
+  # Why didn't I think of this before?
+  all_ids = all_pval$id_cp_interaction # length is 1107
+  unique_ids = unique(all_ids) # length is 1104
+  # Which means there are 3 duplicates. Where are they?
+  to_remove = duplicated(all_ids)
+  # seriously? Just a bunch of TRUEs and FALSEs? Not helpful!
+  dup_row_nums = which(to_remove, arr.ind = TRUE)
+  # Ahd there we are! 637, 655, 850!
   
   
   ## ------------------ ##
